3ed0bd2592e43851e159407b8a8c6b447fd3a922
shakeel shar <shakeelshar07@gmail.com>
2024-03-29 10:24:40 +0500

    more Update

--- program18.js
+++ program18.js
@@ -2,10 +2,14 @@
 
 
 var student = [
-    {name: 'Shakeel Ahmed', age: 25},
-    {name: 'jameel Ahmed', age: 28},
-    {name: 'khalil Ahmed', age: 15},
-    {name: 'adeel Ahmed', age: 25}
+    {name: 'Shakeel Ahmed', age: 25, department : "commputer Science"},
+    {name: 'jameel Ahmed', age: 28,department : "commputer Science"},
+    {name: 'khalil Ahmed', age: 15,department : "commputer Science"},
+    {name: 'adeel Ahmed', age: 25,department : "commputer Science"}
   ];
 
   console.log(student);
+
+  for(a=0;  a<student.length; a++){
+    document.write(student[a].name + " "+ student[a].age+ " "+student[a].department+ "<br>");
+  }
