6d9631432bd381a310a58daa0330ab0ddac6b9e1
shakeel shar <shakeelshar07@gmail.com>
2024-03-30 21:43:40 +0500

    updation

---
+++ program19.js
@@ -0,0 +1,16 @@
+ //   conts variable and array &  objects
+
+
+
+//  const a =[12,23,34,566,79,79,81];
+
+
+//  a[1]=12;
+//  a[0]=23;
+//  a[3]=233;
+//  a[2]="Shakeel Ahmed shar";
+//  a[4]="True";
+
+//  console.log(a);
+
+
---
+++ program20.js
@@ -0,0 +1,4 @@
+var a=(prompt(" Hello dear shakeel Ahmed shar are ?: "));
+
+
+console.log(a);
